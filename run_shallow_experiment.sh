a463356a888ff68a05b58de34aa0964a4365baa2
SonSang <shh1295@gmail.com>
2021-11-27 15:03:27 -0500

    fix num steps

--- run_shallow_experiment.sh
+++ run_shallow_experiment.sh
@@ -7,4 +7,4 @@ echo $checkpoint_name
 echo $num_nodes
 
 # 2 layer
-CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 python scripts/train_agent.py "./runs/${checkpoint_name}_shallow_${num_nodes}_nodes_checkpoints" SB3_ON "$env_name" cuda "{\"ALGO\": \"PPO\", \"policy_kwargs\":{\"features_extractor_class\": \"DenseMlpExtractor\", \"shallow_net_arch\": ${num_nodes}}, \"n_timesteps\": 100000000}" --save_freq=100000
+CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 python scripts/train_agent.py "./runs/${checkpoint_name}_shallow_${num_nodes}_nodes_checkpoints" SB3_ON "$env_name" cuda "{\"ALGO\": \"PPO\", \"policy_kwargs\":{\"features_extractor_class\": \"DenseMlpExtractor\", \"shallow_net_arch\": ${num_nodes}}, \"n_timesteps\": 10000000}" --save_freq=100000
